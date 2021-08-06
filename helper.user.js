f4b70b2b93bff86258d0918638e6db8e389ddaa3
Ch1y0q <qiyue2001@gmail.com>
2021-08-06 22:38:55 +0800

    Update helper.user.js
    ADD for出国党，显示算术均分和GPA（计算通选、军训以外的所有课）

--- helper.user.js
+++ helper.user.js
@@ -1,17 +1,16 @@
 // ==UserScript==
 // @name SEUGPAHelper
 // @description 帮助你快速计算绩点
-// @version 0.1.0
+// @version 0.0.2
 // @match *://ehall.seu.edu.cn/jwapp/sys/cjcx/*
 // @run-at document-end
-// @namespace https://greasyfork.org/users/730743
 // ==/UserScript==
 (function () {
     'use strict';
     function loadMyScript() {
         if (document.querySelector('.bh-advancedQuery-totalNum') === null || document.querySelector('.bh-advancedQuery-totalNum').firstChild === null) { setTimeout(loadMyScript, 500); }
         else {
-            alert('使用方法: 本插件计算所有列表中显示出来的绩点。若想要计算所有课程绩点，请选择务必全部，并务必在右下角选择每页显示100。\n\n使用技巧：在上一步基础上，使用高级搜索，筛选掉所有任选课程，避免课程数超出100导致无法计算。\n\n本插件不计算绩点的课程有军训、中西哲学智慧、中国戏曲艺术、吴健雄学院CPP课，所有任选课，所有通过/不通过课程')
+            //alert('使用方法: 本插件只能计算已有的所有课程绩点（两学期或全部），若想要计算所有课程绩点，请选择务必全部，并务必在右下角选择每页显示100。\n\n使用技巧：在上一步基础上，使用高级搜索，筛选掉所有任选课程，避免课程数超出100导致无法计算。\n\n本插件不计算绩点的课程有军训、中西哲学智慧、中国戏曲艺术、吴健雄学院CPP课，所有任选课，所有通过/不通过课程')
             function getGrade(grade) {
                 if (grade === '优') return 95;
                 if (grade === '良') return 85;
@@ -36,65 +35,85 @@
                 else return 0;
             }
             function judgeLesson(lesson) {
+                if(lesson == null) throw ("Wrong lesson @ judgeLesson");
                 let lessonName = lesson.children[2].firstChild.innerHTML;
                 let lessonFlag = lesson.children[6].firstChild.innerHTML;
+                let lessonCode = lesson.children[3].firstChild.innerHTML;
                 let lessonNotAllowed = ['军训', '中国戏曲艺术', '中西哲学智慧', '算法与程序设计'];
-                if (lessonFlag === '任选') return false;
+                //if (lessonFlag === '任选') return false;
                 if (lessonNotAllowed.indexOf(lessonName) !== -1) return false;
+                if (lessonCode.substring(0,3) === 'B00') return false;//任选课
                 return true;
+            }
+            function isSelective(lesson) {// TODO
+                if(lesson == null) throw ("Wrong lesson @ isSelective");
+                let lessonName = lesson.children[2].firstChild.innerHTML;
+                let lessonFlag = lesson.children[6].firstChild.innerHTML;
+                if (lessonFlag === '任选') return true;
+                return false;
             }
             function calclulate() {
                 let totalNum = parseInt(document.querySelector('.bh-advancedQuery-totalNum').firstChild.innerHTML);
                 let calclulatedNum = 0;
                 let totalGPA = 0;
                 let AVGgpa = 0;
+                let AVGgpaAbroad = 0;
                 let totalGrade = 0;
                 let AVGgrade = 0;
                 let totalCredits = 0;
+
+                let calclulatedNumAbroad = 0;
+                let UnweightedAVGgrade = 0;
+                let totalGPAAbroad = 0;
+                let totalCreditsAbroad = 0;
+                let totalUnweightedGrade = 0;
                 let AllLessons;
                 if (document.querySelector('#contentqb-index-table') !== null) {
                     AllLessons = document.querySelector('#contentqb-index-table').children[1].firstChild.firstChild.children[1].children;
+                    console.log("document.querySelector('#contentqb-index-table') !== null \n");
                 }
                 else {
                     AllLessons = document.querySelector('#contentdqxq-index-table').children[1].firstChild.firstChild.children[1].children;
                 }
-                let htotalGPA=0,htotalGrade=0,htotalCredits=0;
-                for (let i = 0; i < AllLessons.length; i++) {
-                    if(AllLessons[i].children[9].innerHTML !== '通过' && AllLessons[i].children[9].innerHTML !== '不通过' && !isNaN(getGrade(AllLessons[i].children[9].innerHTML))){
-                        calclulatedNum++;
-                        let credit = parseFloat(AllLessons[i].children[7].firstChild.innerHTML);
-                        let grade = getGrade(AllLessons[i].children[9].innerHTML);
-                        let gpa = getGPA(grade);
-                        htotalGPA += gpa * credit;
-                        htotalGrade += grade * credit;
-                        htotalCredits += credit;
-                    }
-                }
-                console.log(`共查询到 ${totalNum}门课程\n计算了 ${calclulatedNum}门课程\n已获总学分为 ${htotalCredits},百分制均分为 ${htotalGrade/htotalCredits}\n4.8制度绩点为 ${htotalGPA/htotalCredits}`);
-                calclulatedNum=0;
-                for (let i = 0; i < AllLessons.length; i++) {
+                for (let i = 0; i < totalNum; i++) {
+                    console.log(i+" \n");
                     if (judgeLesson(AllLessons[i]) && AllLessons[i].children[9].innerHTML !== '通过' && AllLessons[i].children[9].innerHTML !== '不通过') {
-                        calclulatedNum++;
                         let credit = parseFloat(AllLessons[i].children[7].firstChild.innerHTML);
                         let grade = getGrade(AllLessons[i].children[9].innerHTML);
                         let gpa = getGPA(grade);
-                        totalGPA += gpa * credit;
-                        totalGrade += grade * credit;
-                        totalCredits += credit;
+
+                        calclulatedNumAbroad++;
+                        totalUnweightedGrade += grade;
+                        totalCreditsAbroad += credit;
+                        totalGPAAbroad += gpa * credit;
+
+                        if(!isSelective(AllLessons[i])){
+                            calclulatedNum++;
+                            totalGPA += gpa * credit;
+                            totalGrade += grade * credit;
+                            totalCredits += credit;
+                        }
                     }
                 }
-                AVGgrade = totalGrade / totalCredits;
-                AVGgpa = totalGPA / totalCredits;
+                AVGgrade = (totalGrade / totalCredits).toFixed(5);
+                AVGgpa = (totalGPA / totalCredits).toFixed(5);
+
+                UnweightedAVGgrade = (totalUnweightedGrade / calclulatedNumAbroad).toFixed(5);
+                AVGgpaAbroad = (totalGPAAbroad / totalCreditsAbroad).toFixed(5);
                 //alert(`共查询到 ${totalNum}门课程\n计算了 ${calclulatedNum}门课程\n已获总学分为 ${totalCredits}\n百分制均分为 ${AVGgrade}\n4.8制度绩点为 ${AVGgpa}`);
                 let gpaText = document.createElement('div');
-                gpaText.appendChild(document.createTextNode(`共查询到 ${totalNum}门课程,   计算了 ${calclulatedNum}门课程,   已获总学分为 ${totalCredits},   百分制均分为 ${AVGgrade},   4.8制度绩点为 ${AVGgpa}`));
+                let result = `共查询到 ${totalNum}门课程,   计算了 ${calclulatedNum}门课程,   已获总学分为 ${totalCredits},   百分制均分为 ${AVGgrade},   4.8制度绩点为 ${AVGgpa}。\n 出国党：计算了 ${calclulatedNumAbroad}门课程,  已获总学分为 ${totalCreditsAbroad}，   算数均分为 ${UnweightedAVGgrade}，   4.8制绩点为 ${AVGgpaAbroad}`;
+                gpaText.appendChild(document.createTextNode(result));
                 if (document.querySelector('.jqx-tabs-title-container').children.length == 3) { document.querySelector('.jqx-tabs-title-container').appendChild(gpaText); }
-                else {document.querySelector('.jqx-tabs-title-container').children[3].innerHTML=`共查询到 ${totalNum}门课程,   计算了 ${calclulatedNum}门课程,   已获总学分为 ${totalCredits},   百分制均分为 ${AVGgrade},   4.8制度绩点为 ${AVGgpa}`}
+                else {document.querySelector('.jqx-tabs-title-container').children[3].innerHTML=result}
+
+                gpaText.setAttribute('overflow-x', 'scroll');
             }
             let calcBtn = document.createElement('button');
             calcBtn.appendChild(document.createTextNode("计算绩点"));
             calcBtn.onclick = calclulate;
             document.querySelector('.jqx-tabs-title-container').appendChild(calcBtn);
+
         }
     }
     loadMyScript();
